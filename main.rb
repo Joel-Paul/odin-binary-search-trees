8e5675be2cdbff6b0e7729893b3baa3ad2b878a5
Joel Paul <joel.paul@student.unsw.edu.au>
2025-03-04 15:16:33 +1100

    Tie it all together

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 
 require_relative 'lib/bst'
 
-BST = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+# BST = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 # BST.pretty_print
 
 # BST.insert 0
@@ -41,19 +41,44 @@ BST = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 # puts BST.balanced?
 
 # BST.delete 8
-BST.delete 4
+# BST.delete 4
 # BST.delete 3
 # BST.delete 1
-BST.delete 7
+# BST.delete 7
 # BST.delete 5
 # BST.delete 9
 # BST.delete 67
 # BST.delete 23
 # BST.delete 6345
 # BST.delete 324
+# BST.pretty_print
+# puts BST.balanced?
+
+# BST.rebalance
+# BST.pretty_print
+# puts BST.balanced?
+
+# Tie it all together
+BST = Tree.new (Array.new(15) { rand(1..100) })
+BST.pretty_print
+puts BST.balanced?
+
+p BST.level_order
+p BST.preorder
+p BST.postorder
+p BST.inorder
+
+for _ in 1..10
+  BST.insert rand(100..200)
+end
 BST.pretty_print
 puts BST.balanced?
 
 BST.rebalance
 BST.pretty_print
 puts BST.balanced?
+
+p BST.level_order
+p BST.preorder
+p BST.postorder
+p BST.inorder
